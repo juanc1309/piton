925ccf217aefd4aae77e0746e28f9fceee4cf569
juanc1308 <juanc1309@github.com>
2026-02-25 14:11:45 -0600

    codigo de la galleta

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,11 +1,20 @@
 # Programa galleta de la suerte
-print("Esta madre te da algo bien insanso xd")
-porno = input("Ingresa tu madre esa: ")
+dia = input("¿Qué día es hoy? ")
 
 # El if, elif y el else (alv)
-if porno == "verga":
-    print("Que mmd w 🥀")
-elif porno == "ora":
-    print("ora w")
+if dia == "lunes":
+    print("Hoy vas a ser grande")
+elif dia == "martes":
+    print("Todas tus metas se cumpliran")
+elif dia == "miercoles":
+    print("Tu puedes bro")
+elif dia == "jueves":
+    print("Ya eres grande solo falta confiar")
+elif dia == "viernes":
+    print("Eres el mejor")
+elif dia == "sabado":
+    print("Bro eres grande")
+elif dia == "domingo":
+    print("Tu sabes")
 else:
-    print("No dijiste nda w")
+    print("No existe ese día bro.")
