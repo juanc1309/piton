436e72a2945ca1c5fdcc31ad2e8709cc752e716b
juanc <juanc@example.com>
2026-03-04 17:58:18 -0600

    final

--- años_bisiestos.py
+++ años_bisiestos.py
@@ -1,6 +1,6 @@
 print("\nAño bisiesto")
 año = int(input("\nIngrese su número para saber si es bisiesto: "))
-if (((año % 4 == 0) and (año % 100 != 0)) or (año % 400 == 0)):
+if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
     print("El año es bisiesto.")
 else:
     print("El año no es bisiseto.")
