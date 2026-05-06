aac0dfa3c2a9dbd503f4b0ccad9c3b170a067774
juan <148179490+juanc1309@users.noreply.github.com>
2026-05-06 23:57:13 +0000

    Remove unused files and add new scripts for countdown and pyramid patterns

--- 10-secuencia_descendente.py
+++ 10-secuencia_descendente.py
@@ -1,12 +1,3 @@
-for i in range(10):
-    print("La secencia es: ", i)
-
-for i in range(50, 2):
-    print("Con 2 argumento: ", i)
-
-for i in range(10, 50, 2):
-    print("Con 3 argumentos: ", i)
-
 # Pide el número máximo al usuario para que haga una cuenta regresiva
 
 n = int(input("\n\nDame un número máximo para la cuenta regresiva: "))
---
+++ 11-estrella.py
@@ -0,0 +1,4 @@
+num = int(input("Pirámide de emojis, ingrese el número máximo: "))
+for i in range(1,num + 1):
+    print("⭐" * i, end="")
+    print("")
